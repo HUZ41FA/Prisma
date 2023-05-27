ea9701e804231f951ae35df53ad7975389c20018
Huzaifa <huzaifakhanned@gmail.com>
2023-05-28 02:52:39 +0500

    Added Pagination with pagination partial view and created dummy data

--- prisma/blog/views.py
+++ prisma/blog/views.py
@@ -2,13 +2,25 @@
 from django.shortcuts import render, get_object_or_404
 import datetime
 from .models import Post
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 
 
 def post_list(request):
     post_list = Post.objects.all()
-    return render(request, 'blog/posts/list.html', {'post_list': post_list})
+    single_post = Post.objects.get(id=10)
+    paginator = Paginator(post_list, 5)
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+
+
+    return render(request, 'blog/posts/list.html', {'post_list': posts, 'page': page})
 
 
 def post_detail(request, year, month, day, post):
