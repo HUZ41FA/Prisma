a0c4b6ae76bc376194ba338cdbf6e1caeee80008
Huzaifa <huzaifakhanned@gmail.com>
2023-05-28 18:22:18 +0500

    Added Share via email functionality

--- prisma/blog/urls.py
+++ prisma/blog/urls.py
@@ -7,5 +7,6 @@ app_name = 'blog'
 urlpatterns = [
     #path('', view=views.post_list, name='post_list'),
     path('', view=views.PostListView.as_view(), name='post_list'),
+    path('share/<int:post_id>/', view=views.post_share, name='post_share'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/', view=views.post_detail, name='post_detail'),
 ]
--- prisma/blog/views.py
+++ prisma/blog/views.py
@@ -4,9 +4,11 @@ import datetime
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
+from .forms import EmailPostForm
+from django.core.mail import send_mail
 # Create your views here.
 
-
+# A class based view "PostListView" is used instead of this
 def post_list(request):
     post_list = Post.objects.all()
     single_post = Post.objects.get(id=10)
@@ -22,7 +24,6 @@ def post_list(request):
 
     return render(request, 'blog/posts/list.html', {'post_list': posts, 'page': page})
 
-
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post,
                                     publish__year=year,
@@ -31,8 +32,35 @@ def post_detail(request, year, month, day, post):
     
     return render(request, "blog/posts/detail.html", {'post':post})
 
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    form = EmailPostForm()
+    sent = False
+
+    if request.method == "POST":
+        form = EmailPostForm(request.POST)
+
+        if form.is_valid():
+            cd = form.cleaned_data
+            print(cd)
+
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            
+            subject = f"{cd['name']} recommends you read " \
+            f"{post.title}"
+            
+            message = f"Read {post.title} at {post_url}\n\n" \
+            f"{cd['name']}\'s comments: {cd['comments']}"
+            
+            send_mail(subject, message, 'admin@myblog.com',
+            [cd['to']])
+
+            sent = True
+
+    return render(request, 'blog/posts/share.html', {'form': form, 'post' : post, 'sent':sent})
 
 class PostListView(ListView):
+
     queryset = Post.objects.all()
     context_object_name = 'post_list'
     paginate_by = 5
