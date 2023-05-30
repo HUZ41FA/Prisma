b1d0f9d9a5b651e81a057e015a679cb413c0c5e1
Huzaifa <huzaifakhanned@gmail.com>
2023-05-31 00:40:44 +0500

    Markdown support

--- prisma/blog/templatetags/blog_tags.py
+++ prisma/blog/templatetags/blog_tags.py
@@ -1,6 +1,8 @@
 from django import template
 from ..models import Post
 from django.db.models import Count
+from django.utils.safestring import mark_safe
+import markdown
 
 register = template.Library()
 
@@ -18,3 +20,8 @@ def list_latest_posts(count=5):
 @register.simple_tag
 def most_commented_posts(count=5):
     return Post.objects.filter(status="published").annotate(total_comments=Count('comments')).order_by('-total_comments')[:count]
+
+
+@register.filter
+def markdown_format(text):
+    return mark_safe(markdown.markdown(text))
