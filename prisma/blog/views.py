6a86989fc4051cdbc72068f7f2a934b923e60701
Huzaifa <huzaifakhanned@gmail.com>
2023-06-02 19:49:03 +0500

    Weighing Queries

--- prisma/blog/views.py
+++ prisma/blog/views.py
@@ -96,12 +96,12 @@ def post_search(request):
             if form.is_valid():
                 query = form.cleaned_data['query']
 
-                search_vector = SearchVector('title', 'body')
+                search_vector = SearchVector('title', weight='A') + SearchVector('body', weight='B')
                 search_query = SearchQuery(query)
                 results = Post.objects.annotate(
                     search=search_vector,
                     rank=SearchRank(search_vector, search_query),
-                    ).filter(search=search_query).order_by('-rank')
+                    ).filter(rank__gte=0.3).order_by('-rank')
 
     return render(request, 'blog/posts/search.html', {'form':form, 'results':results, 'query':query})
 
